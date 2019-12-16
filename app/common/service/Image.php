e6b51adc2d0b9cd74d6de5752f47ee52d737ef13
xiaye <1005516218@qq.com>
2019-12-16 18:05:57 +0800

    图片操作

--- app/common/service/Image.php
+++ app/common/service/Image.php
@@ -7,8 +7,136 @@
  * Time: 12:36
  */
 namespace app\common\service;
-
+use Intervention\Image\ImageManagerStatic as  Img;
 class Image{
+
+
+
+
+
+
+    public function test($imgpath,$waterpath,$newpath){
+        //创建一个空图片资源
+        $img = Image::canvas(32, 32, '#ff0000');
+        //图片备份
+        $img->backup();
+        //图片返回到备份状态
+        $img->reset();
+        //图像填充
+        $img->fill('tile.png');
+        $img->fill('#ff00ff', 0, 0);
+        //在指定位置画圆
+        $img->circle(10, 100, 100, function ($draw) {
+            $draw->background('#0000ff');
+            $draw->border(1, '#f00');
+        });
+        //在指定位置画椭圆
+        $img->ellipse(60, 120, 100, 100, function ($draw) {
+            $draw->background('#0000ff');
+            $draw->border(1, '#ff0000');
+        });
+        //在指定位置绘制矩形
+        $img->rectangle(5, 5, 195, 195, function ($draw) {
+            $draw->background('rgba(255, 255, 255, 0.5)');
+            $draw->border(2, '#000');
+        });
+        //多点绘制多边形
+        $points = array(
+            40,  50,  // Point 1 (x, y)
+            20,  240, // Point 2 (x, y)
+            60,  60,  // Point 3 (x, y)
+            240, 20,  // Point 4 (x, y)
+            50,  40,  // Point 5 (x, y)
+            10,  10   // Point 6 (x, y)
+        );
+        $img->polygon($points, function ($draw) {
+            $draw->background('#0000ff');
+            $draw->border(1, '#ff0000');
+        });
+        //将文字写入图像
+        $img->text('foo', 0, 0, function($font) {
+            $font->file('foo/bar.ttf');
+            $font->size(24);
+            $font->color('#fdf6e3');
+            $font->align('center');
+            $font->valign('top');
+            $font->angle(45);
+        });
+        //在指定两点之间连线
+        $img->line(10, 10, 100, 10, function ($draw) {
+            $draw->color('#0000ff');
+        });
+        //在指定点绘色
+        $img->pixel('#0000ff', 32, 32);
+        //将图像像素化
+        $img->pixelate(12);
+        //打开一账图片
+        $img = Img::make($imgpath);
+        //隔行扫描  true-打开  false-关闭
+        $img->interlace(true);
+        //获取图片大小
+        $size=$img->filesize();
+        //获取图片mine类型
+        $img->mime();
+        //获取图片高度
+        $img->height();
+        //获取图片宽度
+        $img->width();
+        //获取图片某一点颜色
+        $hexcolor = $img->pickColor(100, 100, 'hex');
+        //图像模糊 0-100默认1
+        $img->blur(15);
+        //图像亮度 -100 100  默认0
+        $img->brightness(35);
+        //图像对比度 -100 100  默认0
+        $img->contrast(65);
+        //图像颜色调整 介于-100 100 之间
+        $img->colorize(-100, 0, 100);
+        //设置图像不透明度 100-0
+        $img->opacity(12);
+        //图片锐化处理
+        $img->sharpen(15);
+        //将图片转化为灰度版本
+        $img->greyscale();
+        //反转当前图像的所有颜色。
+        $img->invert();
+        //自动调整图像方向
+        $img->orientate();
+        //修改图片尺寸
+        $img->resize(320, 240);
+        //调整图像边界大小
+        $img->resizeCanvas(1280, 720, 'center', false, 'ff00ff');
+        //裁剪图片
+        $img->crop(100, 100, 25, 25);
+        //合并裁剪并调整大小，最佳比例裁剪
+        $img->fit(800, 600, function ($constraint) {
+            $constraint->upsize();
+        });
+        //修剪掉图片的一部分
+        trim('top-left', null, 40);
+        //图片增高
+        $img->heighten(100, function ($constraint) {
+            $constraint->upsize();
+        });
+        //图片增宽
+        $img->widen(300, function ($constraint) {
+            $constraint->upsize();
+        });
+        //图片逆时针度数旋转，未覆盖区域填充背景色
+        $img->rotate(-45,'#ffffff');
+        //图片翻转 v-垂直 h-水平
+        $img->flip('v');
+        //添加水印
+        $img->insert($waterpath,'bottom-right',10,10);
+        //图片质量编码转码
+        $img->encode('jpg', 75);
+        //图片缓存
+        Image::cache(function($img) {$img->resize(300, 200)->greyscale();}, 10, true);
+        //保存图片
+        $img->save($newpath,100,'jpg');
+        //销毁实例
+        $img->destroy();
+    }
     /**
      * 大小图合成 例:海报图上放二维码
      * @param $big_img                  大图路径
@@ -133,4 +261,5 @@ class Image{
         $imgout($dst,$path);
         return  $path;
     }
+
 }
